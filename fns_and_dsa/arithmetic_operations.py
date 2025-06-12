2ee909f3c3cf622614b216c6e2f814e62c78b3d5
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-06-12 05:07:38 +0000

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,18 +2,19 @@ def perform_operation(num1, num2, operation):
     """
     A simple match catch calculator
     """
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "Cannot divide by zero"
-            else:
-                return num1 / num2
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation choice. Please choose from add, subtract, multiply, or divide."
 
 
 
