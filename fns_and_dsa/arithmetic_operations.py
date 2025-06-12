7e8c4be57948bbacf5286313ee2e973762ea2427
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-06-12 04:57:54 +0000

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation (num1, num2, operator):
+    """
+    A simple match catch calculator
+    """
+    match operator:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "Cannot divide by zero"
+            else:
+                return num1 / num2
+
+
+
+
+
+
