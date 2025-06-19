e0e4cc1b84370fac5cb839cca24be88779cd4fb4
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-06-19 14:23:58 +0000

    robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,11 +2,11 @@ def safe_divide(numerator, denominator):
 
     try:
         if denominator == 0:
-            raise ZeroDivisionError("Denominator cannot be zero.")
+            raise ZeroDivisionError
         elif not isinstance(numerator, (int, float)) or not isinstance(denominator, (int, float)):
             raise ValueError
         result = float(numerator) / float(denominator)
-        return result
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
