867d966a52a8ef73d06c34b72c6d18b989129517
Elvis Ofori Nkrumah <elvisofori977@gmail.com>
2025-06-19 11:45:01 +0000

    Add files via upload

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def sate_division(numerator, denominator):
+    try:
+        if denominator == 0:
+            raise ZeroDivisionError("Denominator cannot be zero.")
+        elif not isinstance(numerator, (int, float)) or not isinstance(denominator, (int, float)):
+            raise ValueError
+        result = numerator / denominator
+        return result
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
+    except ValueError:
+        return "Both numerator and denominator must be numbers."
+    
